63783c7757e401d81337d04061edd15db9a2b11a
zhangwei <514159799@qq.com>
2016-03-04 15:37:43 +0800

    添加uitemplate

---
+++ Assets/Script/UITemplate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[DisallowMultipleComponent]
+public class UITemplate : MonoBehaviour {
+
+#if UNITY_EDITOR
+    [HideInInspector] public string GUID = string.Empty;
+	[HideInInspector] [System.NonSerialized]public List<GameObject> searPrefabs = new List<GameObject>();
+    public void InitGUID()
+    {
+        if(string.IsNullOrEmpty(GUID))
+        {   
+            GUID = System.Guid.NewGuid().ToString();
+        }
+    }
+#endif
+
+}
