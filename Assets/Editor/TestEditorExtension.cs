70b9434b18a9d880e3e66dd1a952b93a67343a49
zhangwei <514159799@qq.com>
2016-03-15 17:32:40 +0800

    t

---
+++ Assets/Editor/TestEditorExtension.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class TestEditorExtension  {
+
+    [MenuItem("Tools/ObtainGUID")]
+    public static void ObtainGUID()
+    {
+        Debug.Log(AssetDatabase.AssetPathToGUID("Assets/Resources/UITemplate/AAA.prefab"));
+    }
+
+    
+}
