b8b79488f772fd32f5e34db9d08641c7088dd47b
Divad <zw.net.ly@gmail.com>
2016-03-15 21:21:53 +0800

    no message

---
+++ Assets/Editor/EditorExtensionTest.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class EditorExtensionTest  {
+
+	[MenuItem("Test/OjectNamesTest")]
+	static public void ObjectNamesTest()
+	{
+		Debug.Log(ObjectNames.GetClassName(Selection.activeObject));
+		Debug.Log(ObjectNames.GetDragAndDropTitle(Selection.activeObject));
+		Debug.Log(ObjectNames.GetInspectorTitle(Selection.activeObject));
+		Debug.Log(ObjectNames.NicifyVariableName("__tName"));
+		//ObjectNames.SetNameSmart(Selection.activeObject, "ttt");
+	}
+
+	static AnimationClip aniClip = null;
+	[MenuItem("Test/StartAnimation")]
+	static public void StartAnimation()
+	{
+//		if (!AnimationMode.InAnimationMode()) {
+//			AnimationMode.StartAnimationMode();
+//			aniClip = AssetDatabase.LoadAssetAtPath("Assets/Animation/record.anim", typeof(AnimationClip)) as AnimationClip;
+//			AnimationMode.BeginSampling();
+//			AnimationMode.SampleAnimationClip(Selection.activeGameObject,aniClip, 0.0f); 
+//		}
+		AnimationUtility.onCurveWasModified += (AnimationClip clip, EditorCurveBinding binding, AnimationUtility.CurveModifiedType deleted) =>
+		{
+			Debug.Log (clip.name + "  " + binding.path + "       " + binding.propertyName + "  " + deleted.ToString ());
+		};
+	}
+
+	[MenuItem("Test/EndAnimation")]
+	static public void EndAnimation()
+	{
+		if (AnimationMode.InAnimationMode()) {
+			AnimationMode.SampleAnimationClip(Selection.activeGameObject,aniClip, 1.0f); 
+			AnimationMode.EndSampling();
+			AnimationMode.StopAnimationMode();		
+		}
+	}
+
+
+	static EditorExtensionTest()
+	{
+		AnimationUtility.onCurveWasModified += (AnimationClip clip, EditorCurveBinding binding, AnimationUtility.CurveModifiedType deleted) =>
+		{
+			Debug.Log (clip.name + "  " + binding.path + "  " + binding.propertyName + "  " + deleted.ToString ());
+		};
+	}
+
+
+
+}
---
+++ Assets/Editor/HierarchyNameLengthSort.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class HierarchyNameLengthSort : BaseHierarchySort 
+{
+	public override int Compare (GameObject lhs, GameObject rhs)
+	{
+		return lhs.name.CompareTo (rhs.name);
+	}
+
+	public override GUIContent content
+	{
+		get{return new GUIContent("NameLength");}
+	}
+}
