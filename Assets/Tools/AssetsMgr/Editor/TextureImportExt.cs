41157d63cb7a2a24d4f2c12efa292cbdfe0cf376
zw.net.ly@gmail.com <zw.net.ly@gmail.com>
2017-03-01 11:22:03 +0800

    t

--- Assets/Tools/AssetsMgr/Editor/TextureImportExt.cs
+++ Assets/Tools/AssetsMgr/Editor/TextureImportExt.cs
@@ -4,14 +4,14 @@ using UnityEditor;
 
 public class TextureImportExt : AssetPostprocessor
 {
-    void OnPostprocessTexture(Texture2D tx)
-    {
-        var textureImporter = assetImporter as TextureImporter;
-        textureImporter.textureType = TextureImporterType.Advanced;
-        textureImporter.npotScale = TextureImporterNPOTScale.None;
-        textureImporter.mipmapEnabled = false;
-        textureImporter.maxTextureSize = 2048;
+    // void OnPostprocessTexture(Texture2D tx)
+    // {
+    //     var textureImporter = assetImporter as TextureImporter;
+    //     textureImporter.textureType = TextureImporterType.Advanced;
+    //     textureImporter.npotScale = TextureImporterNPOTScale.None;
+    //     textureImporter.mipmapEnabled = false;
+    //     textureImporter.maxTextureSize = 2048;
 
 
-    }
+    // }
 }
